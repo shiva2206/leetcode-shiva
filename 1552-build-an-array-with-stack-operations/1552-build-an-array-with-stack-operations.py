ed09d56a9b857331709bd7fe36efeb620d43fbab
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2023-11-03 08:43:34 -0700

    Time: 42 ms (43.64%), Space: 16.3 MB (44.85%) - LeetHub

--- 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.py
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.py
@@ -1,19 +1,12 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
+        res=[]
+        m=1
+        for i in target:
+            if i>m:res.extend(["Push","Pop"]*(i-m))
+            res.append("Push")
+            m=i+1
+        return res    
 
-        result = []
-        current = 1  # The current number to be pushed.
 
-        for num in target:
-            while current < num:
-                # While the current number is less than the target number,
-                # push the current number and generate the "Push" operation.
-                result.extend(["Push", "Pop"])  # Extend the list with "Push" and "Pop".
-                current += 1
-
-            # The current number matches the target number, so push it.
-            result.append("Push")
-
-            current += 1  # Move to the next number to be pushed.
-
-        return result 
+    
