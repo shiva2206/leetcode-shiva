fb7322a614f54c5ad6051e1ecdf948a0f0cda281
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-03-21 05:43:23 -0700

    Time: 1221 ms (32.45%) | Memory: 102.5 MB (21.13%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.py
+++ 1617-stone-game-iv/stone-game-iv.py
@@ -2,18 +2,15 @@ class Solution:
     def winnerSquareGame(self, n: int) -> bool:
         
         d = {}
-
+        @cache
         def dfs(n):
             if n==1:return True
             if n == 0:return False
 
-            if n in d: return d[n]
-            d[n] = False
-
             j = 1
             while j*j<=n:
                 if not dfs(n- j*j):
-                    d[n] = True 
+                    # d[n] = True 
                     return True
                 j+=1
             return False
