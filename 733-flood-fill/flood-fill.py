66f8096c5c29e1619e8c5a333ef007fe536dc716
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-03-23 01:28:10 -0700

    Time: 66 ms (62.60%) | Memory: 17 MB (5.49%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,18 +1,14 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        if color == image[sr][sc]:return image
-        tar = image[sr][sc]
-
-        deq = deque([(sr,sc)])
-        image[sr][sc]=color
-        while deq:
-            p = len(deq)
-            for _ in range(p):
-                i,j = deq.popleft()
-                for k in [(0,1),(1,0),(-1,0),(0,-1)]:
-                    x = i +k[0]
-                    y = j +k[1]
-                    if 0<=x<len(image) and 0<=y<len(image[x]) and image[x][y] == tar:
-                        image[x][y] = color
-                        deq.append((x,y))
-        return image
+        if image[sr][sc]==color:return image    
+        self.tar = image[sr][sc]
+        def dfs(i,j):
+            if 0<=i<len(image) and 0<=j<len(image[i]) and self.tar == image[i][j]:
+                image[i][j] = color
+                dfs(i+1,j)
+                dfs(i,j+1)
+                dfs(i-1,j)
+                dfs(i,j-1)
+        dfs(sr,sc)        
+        return image        
+                
