b59c0daf32e408c42d886578c402533e95b017f5
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-03-25 23:02:14 -0700

    Time: 601 ms (58.08%) | Memory: 169 MB (22.98%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        d = {}
+        def dfs(i,j):
+            if i>=j:return 0
+            if (i,j) in d:return d[(i,j)]
+
+            if s[i]==s[j]:
+                m = dfs(i+1,j-1)
+            else:
+                m = 1+min(dfs(i,j-1),dfs(i+1,j))
+            d[(i,j)] = m 
+            return m
+        return dfs(0,len(s)-1)            
