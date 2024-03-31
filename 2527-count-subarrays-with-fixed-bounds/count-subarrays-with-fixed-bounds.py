0ca44ab33c0c86cd1141de5ecdb7251966669f11
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-03-30 19:44:04 -0700

    Time: 827 ms (10.74%) | Memory: 31.1 MB (55.65%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
@@ -3,32 +3,36 @@ class Solution:
         
         i = 0
         ans =0
-        d ={}
+        d=[0,0]
         a = inf
         b = -inf
         c = 0
         for j in range(len(nums)):
             if nums[j]>maxK or nums[j]<minK:
                 i = j+1
-                d.clear()
+                d=[0,0]
                 # pq=[]
                 a = inf
                 b = -inf
                 c = 0
                 # l=[]
                 continue
-            if nums[j] not in d:
-                d[nums[j]] = 0
+            if nums[j] ==minK:
+                d[0]+=1
+            if nums[j] == maxK:
+                d[1]+=1
             a = min(a,nums[j])
             b = max(b,nums[j])   
-            d[nums[j]]+=1
+            # d[nums[j]]+=1
             while a == minK and b==maxK:
-                d[nums[i]]-=1
-                if d[nums[i]] == 0:
-                    d.pop(nums[i])
-                if a not in d:
+                if nums[i]==minK:
+                    d[0]-=1
+                if nums[i]==maxK:
+                    d[1]-=1
+               
+                if d[0]==0:
                     a = inf
-                if b not in d:
+                if d[1]==0:
                     b = -inf
                 c +=1  
                 i+=1
