a3ad6981c19004071fba51ebd9331e39935b4925
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-04-17 06:25:00 +0530

    Time: 37 ms (88.13%) | Memory: 17.8 MB (53.12%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.py
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.py
@@ -10,10 +10,10 @@ class Solution:
         self.ans = "z"*13
         def dfs(t,s):
             if not t:return 
-            p = chr(97 + t.val)+s
+            p = s+chr(97 + t.val)
 
             if not t.left and not t.right:
-               self.ans = min(self.ans,p)
+               self.ans = min(self.ans,p[::-1])
             if t.left:
                 dfs(t.left,p)
             if t.right:
