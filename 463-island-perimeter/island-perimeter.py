ff71c3dd5ebbb2219574163492df0e5eef13772d
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-04-18 07:01:54 +0530

    Time: 392 ms (58.12%) | Memory: 18.2 MB (30.93%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,19 +1,22 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         
-        ans = 0
-
+        def dfs(i,j):
+            
+            m= 4
+            grid[i][j]=-1
+            for a,b in [(0,1),(1,0),(-1,0),(0,-1)]:
+                x = i+a
+                y = j+b
+                if x<0 or y<0 or x == len(grid) or y==len(grid[i]):
+                    continue
+                elif grid[x][y]!=0:
+                    m-=1
+                if grid[x][y]==1:
+                    m+=dfs(x,y)
+            return m
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j]==1:
-                    m = 4
-                    for a,b in [(0,1),(1,0),(-1,0),(0,-1)]:
-                        x = a+ i
-                        y = b+j
-
-                        if x<0 or y<0 or x==len(grid) or y == len(grid[i]):
-                            continue
-                        elif grid[x][y]==1:
-                            m-=1
-                    ans+=m
-        return ans
+                    return dfs(i,j)
+        return 0
