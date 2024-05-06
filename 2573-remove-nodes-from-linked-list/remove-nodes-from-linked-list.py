35110cf32efbd0e51aeeeca45065b5425bdc7f79
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-05-06 12:37:38 +0530

    Time: 400 ms (77.73%) | Memory: 63.3 MB (5.81%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -6,21 +6,10 @@
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        l=[]
-        t = head
-        while t:
-            while l and l[-1]<t.val:
-                l.pop()
-            l.append(t.val)
-            t = t.next
-        ans = ListNode(0)
-        t = ans
-        i = 0
-        while i<len(l):
-            if l[i] == head.val:
-                ans.next = head
-                ans = ans.next
-                i+=1
-
-            head = head.next
-        return t.next         
+        if not head or not head.next:
+            return head
+        l = self.removeNodes(head.next)
+        if head.val >=l.val:
+            head.next = l
+            return head
+        return l
