515306202fe698cd9dea8143b50c1754d9ba31a5
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-05-23 18:46:52 +0530

    Time: 51 ms (11.94%) | Memory: 18.3 MB (15.28%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -2,20 +2,22 @@ class Solution:
     def numTilings(self, n: int) -> int:
         d={}
         def dfs(a,b):
-            if a>n or b>n:return 0
+            if a>n:return 0
             if a == b == n:return 1
+            
+            if b>a:
+                a,b = b,a
 
             if (a,b) in d:return d[(a,b)]
-            if (b,a) in d:return d[(b,a)]
+            # if (b,a) in d:return d[(b,a)]
 
             m = 0
 
             if a==b:
                 m = dfs(a+2,b+2) + dfs(a+2,b+1) + dfs(a+1,b+2) +dfs(a+1,b+1)
-            elif a-b==1:
-                m+= dfs(a,b+2) + dfs(a+1,b+2)
             else:
-                m+= dfs(a+2,b) + dfs(a+2,b+1)
+                m+= dfs(a,b+2) + dfs(a+1,b+2)
+         
             d[(a,b)]=m%1000000007
             return d[(a,b)]
         a=  dfs(0,0)
