f23a33a8a44cc8fd4935e82d934c68568d598324
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-06-06 06:42:17 +0530

    Time: 157 ms (78.18%) | Memory: 18.4 MB (50.76%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,30 +1,25 @@
 class Solution:
-    def isNStraightHand(self, hand: List[int], grp: int) -> bool:
-        if len(hand)%grp:return False
-        d={}
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        
+        d = {}
+        pq = []
         for i in hand:
             if i not in d:
-                d[i]=0
-            d[i]+=1
-
-        l = []
-        for i in d:
-            l.append((i,d[i]))
-        heapq.heapify(l)
-
-        while l:
-            z = []
-            x,y = heapq.heappop(l)
-            if y>1:
-                z.append((x,y-1))
-            for i in range(grp-1):
-                if not l:return False
-                a,b = heapq.heappop(l)
-                if a-1!=x:return False
-                if b>1:
-                    z.append((a,b-1))
-                x = a
-            for j in z:
-                heapq.heappush(l,j)    
-        return True                    
+                d[i]= 1
+                heapq.heappush(pq,i)
+            else:
+                d[i]+=1
+        
+        while pq:
+            x = pq[0]
+            for i in range(groupSize):
+                y = x + i
+                if y not in d: return False
+                d[y]-=1
+                if d[y]==0:
+                    d.pop(y)
+            while pq and pq[0] not in d:
+                # d.pop(pq[0])
+                heapq.heappop(pq)
+        return True
 
