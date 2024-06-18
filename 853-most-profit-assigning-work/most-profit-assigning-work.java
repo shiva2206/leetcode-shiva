ba70175eca3f172580d86557e909d8dbee54539a
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-06-18 08:12:25 +0530

    Time: 32 ms (30.22%) | Memory: 45.2 MB (93.73%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxProfitAssignment(int[] diff, int[] prof, int[] work) {
+        int[][] l = new int[diff.length][2];
+        for(int i =0;i<diff.length;i++){
+            l[i][0] = diff[i];
+            l[i][1] = prof[i];
+        }
+
+        Arrays.sort(l,new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a,int[] b){
+                return Integer.compare(a[0],b[0]);
+            }
+        });
+
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a,b)->b-a);
+
+        int ans = 0,j=0;
+        Arrays.sort(work);
+        for(int i = 0;i<work.length;i++){
+            while (j<l.length && work[i]>=l[j][0]){
+                pq.add(l[j][1]);
+                j+=1;
+            }
+            if(pq.size()>0)ans += pq.peek();
+        }
+        return ans;
+
+
+
+    }
+}
