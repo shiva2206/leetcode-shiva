f3e1d3b2b3f9a768099c8535822ce98c38e1c2cb
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-07-02 08:32:41 +0530

    Time: 50 ms (48.69%) | Memory: 16.9 MB (9.42%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -5,13 +5,9 @@ class Solution:
             if i not in d:
                 d[i]=0
             d[i]+=1
-        g = {}
-        for i in nums2:
-            if i not in g:
-                g[i]=0
-            g[i]+=1
         res = []
-        for i in d:
-            if i in g:
-                res.extend([i]*min(d[i],g[i]))
+        for i in nums2:
+            if i in d and d[i]>0:
+                res.append(i)
+                d[i]-=1
         return res
