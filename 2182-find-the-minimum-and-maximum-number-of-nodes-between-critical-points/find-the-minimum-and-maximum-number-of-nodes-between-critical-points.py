83457b34918be78d4880d39a47da80234f2bcbd1
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-07-05 08:05:03 +0530

    Time: 367 ms (33.46%) | Memory: 44.5 MB (14.45%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -5,19 +5,35 @@
 #         self.next = next
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
-        l = []
+        
+        start = -1
+        m = float('inf')
+        p = -1
         prev = None
         b = 0
         while head:
             b+=1
             if prev and head.next:
                 if prev.val<head.val>head.next.val:
-                    l.append(b)
-                    
+                    if start == -1:
+                        start = b
+                    else:
+                        m = min(m,b-p)
+                        
+                    p = b
                 if prev.val>head.val<head.next.val:
-                    l.append(b)
+                    if start == -1:
+                        start = b
+                    else:
+                        m = min(m,b-p)
+                    p = b
             prev = head
             head = head.next
+        if start == p :return [-1,-1]
+        return [m,p-start]
+
+
+           
         if len(l)<2:return [-1,-1]
         l.sort()
         # print(l)
