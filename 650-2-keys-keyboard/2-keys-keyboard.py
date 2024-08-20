f9d37b495b5549011e85cd250a25400048ba0a09
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-08-20 19:30:25 +0530

    Time: 191 ms (36.85%) | Memory: 33.3 MB (15.53%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -1,16 +1,12 @@
 class Solution:
     def minSteps(self, n: int) -> int:
         
-        d={}
-        def dfs(i,p):
-            if i==n:return 0
-            if i>=n: return float('inf')
-
-            if (i,p) in d:return d[(i,p)]
-
-            if p==0:
-                d[(i,p)]=2+dfs(i+i,i)
-            else:    
-                d[(i,p)]=1+min(dfs(i+p,p),1+dfs(i+i,i))
-            return d[(i,p)]
-        return dfs(1,0)  
+        d ={}
+        def dfs(i,j):
+            if i == n:return 0
+            if i>n:return float('inf')
+            if (i,j) in d:return d[(i,j)]
+            d[(i,j)] = float('inf')
+            d[(i,j)] = 1 + min(dfs(i,i),dfs(i+j,j))
+            return d[(i,j)]
+        return dfs(1,0)
