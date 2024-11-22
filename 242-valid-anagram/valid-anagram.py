83d13dbfd5bdcf7d50d495a765939b9f83db32dc
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-11-22 15:20:20 +0530

    Time: 9 ms (79.23%) | Memory: 16.7 MB (89.75%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        arr = [0]*26
+        for i in range(len(s)):
+            arr[ord(s[i])-97]+=1
+            arr[ord(t[i])-97]-=1
+        for i in arr:
+            if i!=0:return False
+        return True
+        
+        
