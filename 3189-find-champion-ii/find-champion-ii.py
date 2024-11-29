71e729ed2ca22a1ce00a52fccddc3a8084b5289f
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-11-29 21:50:01 +0530

    Time: 10 ms (47.35%) | Memory: 18.2 MB (45.32%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.py
+++ 3189-find-champion-ii/find-champion-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
         arr = [0]*n
+        ans = n
         for i,j in edges:
             arr[j]+=1
-        ans = -1
+            if arr[j]==1:
+                ans-=1
+        if ans!=1:return -1
         for i in range(n):
-            if arr[i]==0:
-                if ans==-1:
-                    ans=i
-                else:
-                    return -1
-        return ans
+            if arr[i]==0:return i
+        return -1
+     
