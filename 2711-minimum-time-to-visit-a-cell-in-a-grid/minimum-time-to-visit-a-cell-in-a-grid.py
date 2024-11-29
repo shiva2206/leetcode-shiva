fc77822d9c874ba552bc56ef82c6b535c2f81b0e
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-11-29 05:54:27 +0530

    Time: 1023 ms (54.42%) | Memory: 30.5 MB (82.85%) - LeetSync

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
@@ -5,7 +5,7 @@ class Solution:
         if len(grid)==1 and grid[0][1]>1:return -1
         if len(grid[0])==1 and grid[1][0]>1:return -1
         if grid[0][1]>1 and grid[1][0]>1:return -1
-        vis = set([(0,0)])
+        
         while l:
             val,x,y = heapq.heappop(l)
             if x==len(grid)-1 and y== len(grid[0])-1:return val
@@ -14,16 +14,15 @@ class Solution:
 
                 i = a+x
                 j = b+y
-                if i<0 or j<0 or i==len(grid) or j== len(grid[i]) or (i,j) in vis :continue
-                
-                vis.add((i,j))
+                if i<0 or j<0 or i==len(grid) or j== len(grid[i]):continue
+                if grid[i][j]<0 : continue
                 m = val +1
                 if m<grid[i][j]:
                     if (grid[i][j]-m)%2==1:
                         m = grid[i][j] +1
                     else:
                         m = grid[i][j]
-                grid[i][j] = -m
+                grid[i][j] = -float('inf') if m==0 else -m
                 heapq.heappush(l,(m,i,j))
         print("joke")
         return -1
