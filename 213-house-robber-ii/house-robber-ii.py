282e4737ff8dbe4d649abc5ce3ed3ee9d9159b99
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-12-01 12:43:09 +0530

    Time: 1 ms (17.72%) | Memory: 17.3 MB (5.83%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums)==0:return 0
+        if len(nums)==1:return nums[0]
+        d = {}
+        
+        def dfs(i):
+            if i>=len(nums):return 0
+            if i in d:return d[i]
+            d[i] = max(nums[i] + dfs(i+2),dfs(i+1))
+            return d[i]
+        p = nums.pop()
+        ans = dfs(0)
+        d={}
+        nums.append(p)
+        ans = max(ans,dfs(1))
+        return ans
