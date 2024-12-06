19a8276ac63671ee495eea7ef79b1766c4f87547
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-12-06 06:47:47 +0530

    Time: 75 ms (29.53%) | Memory: 20.8 MB (15.71%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -13,4 +13,22 @@ class Solution:
                 m =1 + min(dfs(i+1,j),dfs(i,j+1),dfs(i+1,j+1))
             d[(i,j)] = m 
             return m
-        return dfs(0,0)
+
+        d = [[0]*(len(word2)+1) for i in range(len(word1)+1)]
+        for i in range(len(word1),-1,-1):
+            for j in range(len(word2),-1,-1):
+                if j == len(word2):
+                    d[i][j] = len(word1)-i
+                    continue
+                if i ==len(word1):
+                    d[i][j] = len(word2) - j
+                    continue
+                m = 0
+                if word1[i] == word2[j]:
+                    m = d[i+1][j+1]
+                else:
+                    m =1 + min(d[i+1][j],d[i][j+1],d[i+1][j+1])
+                d[i][j] = m
+
+
+        return d[0][0]
