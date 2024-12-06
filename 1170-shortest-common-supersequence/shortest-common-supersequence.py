1e52bd41d54c530c0ea6ce6b080d5e1d7ca89ace
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-12-06 05:44:30 +0530

    Time: 511 ms (36.85%) | Memory: 66.6 MB (24.72%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.py
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.py
@@ -1,35 +1,51 @@
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
-        d=defaultdict(lambda : -1)
-        def dfs(i,j):
-            if i == len(str1) or j==len(str2):return 0
-            if (i,j) in d:return d[(i,j)]
+        
+        # d = {}
+        # def dfs(i,j):
+        #     if i == len(str1): return len(str2) - j
+        #     if j == len(str2):return len(str1) - i
+        #     if (i,j) in d: return d[(i,j)]
+        #     m = 0
+        #     if str1[i] == str2[j]:
+        #         m = 1 + dfs(i+1,j+1)
+        #     else:
+        #         m = 1 + min(dfs(i,j+1),dfs(i+1,j))
+        #     d[(i,j)] =m
+        #     return m
+        #  ans =  dfs(0,0)
+        d = [[0]*(len(str2)+1) for i in range(len(str1)+1)]
+        for i in range(len(str1)):
+            d[i][len(str2)] = len(str1)-i
 
-            if str1[i] == str2[j]:
-                m = 1 + dfs(i+1,j+1)
-            else:
-                m = max(dfs(i+1,j),dfs(i,j+1))
+        for j in range(len(str2)):
+            d[len(str1)][j] = len(str2)-j 
 
-            d[(i,j)] = m 
-            return m
-        p = dfs(0,0)
-        res =""
+        for i in range(len(str1)-1,-1,-1):
+            for j in range(len(str2)-1,-1,-1):
+                if str1[i] == str2[j]:
+                        m = 1 + d[i+1][j+1]
+                else:
+                        m = 1 + min(d[i][j+1],d[i+1][j])
+                d[i][j] =m
+                
+        print(d)
+        res = ""
+
         i = 0
         j = 0
-
         while i<len(str1) and j<len(str2):
             if str1[i] == str2[j]:
                 res += str1[i]
                 i+=1
                 j+=1
-            elif d[(i,j+1)]<d[(i+1,j)]:
+            elif d[i][j+1] < d[i+1][j]:
+                res += str2[j]
+                j+=1
+            else:
                 res += str1[i]
                 i+=1
-            else:
-                res +=str2[j]
-                j+=1
-        if i<len(str1):
-            res+=str1[i:]
-        else:
-            res+=str2[j:]            
-        return res            
+            print(res,i,j)
+        
+        print(str1[i:],str2[j:])
+        return res + str1[i:] + str2[j:]
