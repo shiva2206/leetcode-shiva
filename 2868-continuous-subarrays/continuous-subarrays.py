8a4cf7ec9d4912d1277ac5f42b3634b8899453d0
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-12-14 23:00:58 +0530

    Time: 604 ms (82.23%) | Memory: 27.9 MB (17.48%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.py
+++ 2868-continuous-subarrays/continuous-subarrays.py
@@ -1,32 +1,26 @@
 class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
         
-        i = 0
-        
-        maxque = deque()
-        minque =deque()
         ans = 0
-        j = 0
+        minque = deque()
+        maxque = deque()
+
+        i=0
         for j in range(len(nums)):
-            # while maxque and i>maxque[0]:
-            #     maxque.popleft()
-            # while minque and i>minque[0]:
-            #     minque.popleft()
-            
-            while maxque and nums[maxque[-1]] < nums[j]:
-                maxque.pop()
-            while minque and nums[minque[-1]] > nums[j]:
+            while minque and nums[minque[-1]]>nums[j]:
                 minque.pop()
+            
+            while maxque and nums[maxque[-1]]<nums[j]:
+                maxque.pop()
+            
             minque.append(j)
             maxque.append(j)
-            
-            while (nums[maxque[0]]- nums[minque[0]])>2:
-                # print(i)
+
+            while nums[maxque[0]] - nums[minque[0]] > 2:
                 i+=1
-                if maxque and i>maxque[0]:
+                if maxque and i>maxque[0]: 
                     maxque.popleft()
                 if minque and i>minque[0]:
                     minque.popleft()
             ans += j-i+1
-            # print(i,j,minque,maxque,j-i+1)
         return ans
