66ffe76745b6556a49e32b0bae7c0ee5d024361c
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-12-19 06:39:02 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (48.15%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -1,20 +1,19 @@
 class Solution {
-    HashMap<Integer,Integer> d = new HashMap<>();
+    
     public int maxChunksToSorted(int[] arr) {
-        return (int)dfs(0,arr);
-    }
+       
 
-    Integer dfs(int i,int[] arr){
-        if(i==arr.length) return 0;
-        if(d.containsKey(i)) return d.get(i);
-        int m = 0;
-        int s = 0,req=0;
-        for(int j = i;j<arr.length;j++){
-            s += arr[j];
-            req+= j;
-            if(req == s) m = Math.max(m , 1 + dfs(j+1,arr) );
+        int[] d = new int[arr.length+1];
+        for(int i = arr.length - 1;i>=0;i--){
+            int req = 0 , s= 0;
+            for(int j = i;j<arr.length;j++){
+                req+= j;
+                s+=arr[j];
+                if(req==s) d[i] = Math.max(d[i],1 + d[j+1]);
+            }
         }
-        d.put(i,m);
-        return m;
+        return d[0];
     }
 }
+
+   
