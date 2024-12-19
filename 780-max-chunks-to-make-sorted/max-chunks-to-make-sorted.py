204f335f8ca20275d575e97b0862a919442b6ae7
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-12-19 06:28:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (14.45%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
@@ -1,18 +1,31 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         
-        d= {}
-        def dfs(i):
-            if i == len(arr):return 0
-            if i in d:return d[i]
-            d[i] = -len(arr)
+        # d= {}
+        # def dfs(i):
+        #     if i == len(arr):return 0
+        #     if i in d:return d[i]
+        #     d[i] = -len(arr)
+        #     req = 0
+        #     s = 0
+        #     for j in range(i,len(arr)):
+        #         s += arr[j]
+        #         req += j
+        #         if req == s:
+        #             d[i]  = max(d[i],1 + dfs(j+1))
+        #     return d[i]
+        
+
+
+        # return dfs(0)
+
+        d = [0]*(1+len(arr))
+        for i in range(len(arr)-1,-1,-1):
             req = 0
             s = 0
             for j in range(i,len(arr)):
                 s += arr[j]
                 req += j
                 if req == s:
-                    d[i]  = max(d[i],1 + dfs(j+1))
-            return d[i]
-        
-        return dfs(0)
+                    d[i] = max(d[i],1 + d[j+1])
+        return d[0]
