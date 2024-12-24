286efe3270ca5ed34cdbe0f9b71e54ee18df2ec0
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-12-24 17:17:41 +0530

    Time: 709 ms (31.03%) | Memory: 118.2 MB (20.69%) - LeetSync

---
+++ 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minimumDiameterAfterMerge(self, edges1: List[List[int]], edges2: List[List[int]]) -> int:
+        
+        def adjlist(edges):
+            d = {}
+            for i,j in edges:
+                if i not in d:
+                    d[i] =[]
+                if j not in d:
+                    d[j]=[]
+                d[i].append(j)
+                d[j].append(i)
+            return d
+        adj1 = adjlist(edges1)
+        adj2 = adjlist(edges2)
+
+        def findmax(adj):
+            k = 0
+            def dfs(child,par):
+                nonlocal k
+                if child not in adj:return 0
+                max_child = [0,0]
+                for c in adj[child]:
+                    if c == par:continue
+                    heapq.heappush(max_child,dfs(c,child))
+                    heapq.heappop(max_child)
+                k = max(k,sum(max_child))
+        
+                return max(max_child)+1 
+            dfs(0,-1)
+            return k
+        d1 = findmax(adj1)
+        d2 = findmax(adj2)
+        
+        return max(d1,d2,1 + math.ceil(d1/2) + math.ceil(d2/2))
+
+
+                
+
