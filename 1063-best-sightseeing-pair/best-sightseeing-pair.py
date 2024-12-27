e1cce53fa59ab1f4351242025e19a0b3dd71ac94
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-12-27 08:30:53 +0530

    Time: 61 ms (80.00%) | Memory: 23.4 MB (10.49%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -1,17 +1,10 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         
-        plus = []
-        minus = []
-        for i in range(len(values)):
-            plus.append(values[i]+i)    
-            minus.append(values[i]-i)
-        
-        m = minus[-1]
-        for i in range(len(minus)-2,-1,-1):
-            m = max(minus[i],m)
-            minus[i] = m
-        ans = 0
-        for i in range(len(plus)-1):
-            ans = max(ans,plus[i]+minus[i+1])
-        return ans
+       m = values[-1] - (len(values)-1)
+       ans = 0
+       for i in range(len(values)-2,-1,-1):
+           ans =max(ans,values[i] + i  + m)
+           m = max(m,values[i]-i)
+       return ans
+              
