343b02c35bf54d410285d9e3f427aaea7c151543
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-12-31 09:53:23 +0530

    Time: 7 ms (44.54%) | Memory: 18.7 MB (5.96%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -1,6 +1,18 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         l = set(days)
+        
+        d = {}
+        def dfs(i):
+            if i>days[-1]:return 0
+            if i in d:return d[i]
+            if i not in l: return dfs(i+1)
+            d[i] = min(costs[0] + dfs(i+1),costs[1] + dfs(i+7), costs[2] + dfs(i+30))
+            return d[i]
+        return dfs(0)
+        
+        
+        
         d = [0]
         for i in range(1,days[-1]+1):
             if i not in l:
