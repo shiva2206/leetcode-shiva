18d9d53708d50d6bb6f87b25517e5d6e52a4642f
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2024-12-31 09:26:04 +0530

    Time: 275 ms (5.67%) | Memory: 18.6 MB (6.27%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -2,16 +2,19 @@ class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         
 
-        d = {}
-        def dfs(i,j):
-            if j == len(days):return 0
-            if i>=days[j]:return dfs(i,j+1)
-            if (i,j) in d:return d[(i,j)]
-            d[(i,j)] = float('inf')
-            m = min(costs[0] + dfs(i+1,j),costs[1] + dfs(i+7,j) ,costs[2] + dfs(i+30,j),dfs(days[j]-1,j))
-            d[(i,j)] = m 
-            return m
-        return dfs(0,0)
+     
+
+        d = [[0]*(len(days)+1) for i in range(days[-1]+31)]
+
+        for i in range(days[-1]-1,-1,-1):
+            for j in range(len(days)-1,-1,-1):
+                if i>=days[j]:
+                    d[i][j] = d[i][j+1]
+                else:
+                    d[i][j] = float('inf')
+                    m = min(costs[0] + d[i+1][j],costs[1] + d[i+7][j] ,costs[2] + d[i+30][j],d[days[j]-1][j])
+                    d[i][j] = m
+        return d[0][0]
             
 
 
