8204ed1f74a0058800162a1f85fc1475efc6500d
G Shiva Prasad <81207554+shiva2206@users.noreply.github.com>
2025-05-13 04:45:52 +0530

    Time: 31 ms (26.20%) | Memory: 18 MB (35.47%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,21 +1,22 @@
 class Solution:
-    def combinationSum2(self, cand: List[int], tar: int) -> List[List[int]]:
+    def combinationSum2(self, cand: List[int], target: int) -> List[List[int]]:
         
         cand.sort()
-        res = []
+        ans = []
         s = []
         def dfs(i,t):
-            if t==tar:
-                res.append(s.copy())
-                return 
-            if i==len(cand) or t>tar:
+            if t == target:
+                ans.append(s.copy())
                 return
+
+            if t> target or i == len(cand):
+                return 
             s.append(cand[i])
-            dfs(i+1,t+cand[i])
+            dfs(i+1,t + cand[i])
             s.pop()
-            x=i
-            while x+1<len(cand) and cand[x+1]==cand[i]:
-                x+=1
-            dfs(x+1,t)
+            x = cand[i]
+            while i<len(cand) and cand[i] == x:
+                i+=1
+            dfs(i,t)
         dfs(0,0)
-        return res
+        return ans
